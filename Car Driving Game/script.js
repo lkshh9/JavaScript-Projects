0eb4600c0f26f4757e8f683c6643f8c5d1419a04
lokesh <lokeshmarwal007@gmail.com>
2024-05-31 23:25:00 +0530

    car driving project

---
+++ Car Driving Game/script.js
@@ -0,0 +1,48 @@
+const score = document.querySelector(".score");
+const startScreen = document.querySelector(".startScreen");
+const gameArea = document.querySelector(".gameArea");
+let player = {speed:5};
+
+let keys = {ArrowUp:false,ArrowdDown:false,ArrowRight:false,ArrowLeft:false,wLeft:false};
+
+startScreen.addEventListener("click",start);
+document.addEventListener("keydown",pressOn);
+document.addEventListener("keyup",pressOff);
+
+function playGame(){
+  console.log("inplay");
+  let car = document.querySelector(".car");
+  if(player.start){
+    if(keys.ArrowUp){player.y += player.speed;}
+    if(keys.ArrowDown){player.y -= player.speed;}
+    if(keys.ArrowLeft){player.x -= player.speed;}
+    if(keys.ArrowRight){player.x += player.speed;}
+    window.requestAnimationFrame(playGame);
+  }
+ }
+
+function pressOn(e){
+  e.preventDefault();
+  keys[e.key] = true;
+  console.log(keys);
+}
+
+function pressOff(e){
+  e.preventDefault();
+  keys[e.key] = false;
+  console.log(keys);
+}
+
+function start(){
+  startScreen.classList.add("hide");
+  gameArea.classList.remove("hide");
+  player.start=true;
+  window.requestAnimationFrame(playGame);
+  let car = document.createElement("div");
+  car.innerText = "Car";
+  car.setAttribute("class","car");
+  gameArea.appendChild(car);
+  player.x = car.offsetLeft;
+  player.y = car.offsetTop;
+  console.log(player);
+}
